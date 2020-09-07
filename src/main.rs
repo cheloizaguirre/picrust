7ffb3d2dcc329dc56291dd153a24f743302ef39e
Marcelo Izaguirre <uchiha.chelo@gmail.com>
2020-09-07 10:28:08 +0300

    Renders empty squares for a puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use bevy::prelude::*;
 use array2d::Array2D;
+use bevy::prelude::*;
 use rand::Rng;
 
 #[derive(Clone, Debug, Copy)]
 enum SquareState {
     Filled,
-    Empty
+    Empty,
+}
+
+impl Default for SquareState {
+    fn default() -> Self {
+        SquareState::Empty
+    }
 }
 
 fn create_random_puzzle(rows: u32, columns: u32) -> Puzzle {
     let mut rng = rand::thread_rng();
-    let random = || {
-        match rng.gen_range(0, 2) {
-            0 => Square { solution: SquareState::Empty, current: SquareState::Empty },
-            _ => Square { solution: SquareState::Filled, current: SquareState::Empty },
-        }
+    let random = || match rng.gen_range(0, 2) {
+        x if x >= 1 => Square {
+            solution: SquareState::Filled,
+            ..Default::default()
+        },
+        _ => Square {
+            ..Default::default()
+        },
     };
-    Puzzle(Array2D::filled_by_row_major(random, rows as usize, columns as usize))
+    Puzzle(Array2D::filled_by_row_major(
+        random,
+        rows as usize,
+        columns as usize,
+    ))
 }
 
-fn startup_puzzle(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>){
+fn startup_puzzle(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut textures: ResMut<Assets<Texture>>,
+    asset_server: Res<AssetServer>,
+) {
     let (rows, cols) = (5, 5);
     let random_puzzle = create_random_puzzle(rows, cols);
     let puzzle_border_material = materials.add(Color::rgb(0.2, 0.2, 1.0).into());
@@ -64,15 +82,41 @@ fn startup_puzzle(mut commands: Commands, mut materials: ResMut<Assets<ColorMate
             },
             ..Default::default()
         });
+
+    let empty_texture_handle = asset_server.load_sync(&mut textures, "assets/empty_square.png").unwrap();
+    let square_size = Vec2::new(SQUARE_SIZE, SQUARE_SIZE);
+    for (row_i, row) in random_puzzle.0.rows_iter().enumerate() {
+        for (col_i, _square) in row.enumerate() {
+            let position = Vec3::new(col_i as f32 * SQUARE_SIZE - bounds.x() / 2.0, row_i as f32 * SQUARE_SIZE - bounds.y() / 2.0, 0.0);
+            commands.spawn(SpriteComponents {
+                material: materials.add(empty_texture_handle.into()),
+                sprite: Sprite { size: square_size },
+                scale: Scale(SQUARE_SIZE / 128.0),
+                translation: Translation(position),
+                ..Default::default()
+            });
+        }
+    }
 }
 
-struct Puzzle (Array2D<Square>);
+struct Puzzle(Array2D<Square>);
 const SQUARE_SIZE: f32 = 100.0;
 
 #[derive(Debug, Clone, Copy)]
 struct Square {
     solution: SquareState,
-    current: SquareState
+    current: SquareState,
+    rel_pos: Vec2,
+}
+
+impl Default for Square {
+    fn default() -> Self {
+        Square {
+            solution: SquareState::Empty,
+            current: SquareState::Empty,
+            rel_pos: Vec2::new(0.0, 0.0),
+        }
+    }
 }
 
 fn main() {
