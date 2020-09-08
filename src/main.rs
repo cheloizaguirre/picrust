d417b898eccccda3179a12bb02f855a6df14c850
Marcelo Izaguirre <uchiha.chelo@gmail.com>
2020-09-08 10:37:05 +0300

    Adds basic UI stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::pass::ClearColor};
 mod puzzle;
 mod square;
+mod ui_elements;
 use crate::square::SQUARE_SIZE;
+use crate::ui_elements::UIPlugin;
 
 fn startup_puzzle(
     mut commands: Commands,
@@ -20,26 +22,38 @@ fn startup_puzzle(
         .unwrap();
     let square_size = Vec2::new(SQUARE_SIZE, SQUARE_SIZE);
     for (row_i, row) in random_puzzle.squares.rows_iter().enumerate() {
-        for (col_i, _square) in row.enumerate() {
+        for (col_i, square) in row.enumerate() {
             let position = Vec3::new(
                 col_i as f32 * SQUARE_SIZE - bounds.x() / 2.0,
                 row_i as f32 * SQUARE_SIZE - bounds.y() / 2.0,
                 0.0,
             );
-            commands.spawn(SpriteComponents {
-                material: materials.add(empty_texture_handle.into()),
-                sprite: Sprite { size: square_size },
-                scale: Scale(SQUARE_SIZE / 128.0),
-                translation: Translation(position),
-                ..Default::default()
-            });
+            commands
+                .spawn(SpriteComponents {
+                    material: materials.add(empty_texture_handle.into()),
+                    sprite: Sprite { size: square_size },
+                    scale: Scale(SQUARE_SIZE / 128.0),
+                    translation: Translation(position),
+                    ..Default::default()
+                })
+                .with(square.clone());
         }
     }
 }
 
 fn main() {
     App::build()
+        .add_resource(WindowDescriptor {
+            title: "Picrust".to_string(),
+            width: 1920,
+            height: 1080,
+            vsync: true,
+            resizable: true,
+            ..Default::default()
+        })
+        .add_resource(ClearColor(Color::BLACK))
         .add_default_plugins()
+        .add_plugin(UIPlugin)
         .add_startup_system(startup_puzzle.system())
         .run();
 }
---
+++ src/ui_elements.rs
@@ -0,0 +1,94 @@
+use bevy::prelude::*;
+pub struct UIPlugin;
+
+impl Plugin for UIPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<ButtonMaterials>()
+            .add_startup_system(setup_ui.system())
+            .add_system(ui_system.system());
+    }
+}
+
+struct ButtonMaterials {
+    normal: Handle<ColorMaterial>,
+    hovered: Handle<ColorMaterial>,
+    pressed: Handle<ColorMaterial>,
+}
+
+impl FromResources for ButtonMaterials {
+    fn from_resources(resources: &Resources) -> Self {
+        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
+        ButtonMaterials {
+            normal: materials.add(Color::rgb(0.02, 0.02, 0.02).into()),
+            hovered: materials.add(Color::rgb(0.05, 0.05, 0.05).into()),
+            pressed: materials.add(Color::rgb(0.1, 0.5, 0.1).into()),
+        }
+    }
+}
+
+fn ui_system(
+    button_materials: Res<ButtonMaterials>,
+    mut interaction_query: Query<(
+        &Button,
+        Mutated<Interaction>,
+        &mut Handle<ColorMaterial>,
+        &Children,
+    )>,
+    text_query: Query<&mut Text>,
+) {
+    for (_button, interaction, mut material, children) in &mut interaction_query.iter() {
+        let mut text = text_query.get_mut::<Text>(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.value = "Press".to_string();
+                *material = button_materials.pressed;
+            }
+            Interaction::Hovered => {
+                text.value = "Hover".to_string();
+                *material = button_materials.hovered;
+            }
+            Interaction::None => {
+                text.value = "Button".to_string();
+                *material = button_materials.normal;
+            }
+        }
+    }
+}
+fn setup_ui(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    button_materials: Res<ButtonMaterials>,
+) {
+    commands
+        // ui camera
+        .spawn(UiCameraComponents::default())
+        .spawn(ButtonComponents {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            material: button_materials.normal,
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn(TextComponents {
+                text: Text {
+                    value: "Button".to_string(),
+                    font: asset_server
+                        .load("assets/fonts/Meslo LG S DZ Regular for Powerline.ttf")
+                        .unwrap(),
+                    style: TextStyle {
+                        font_size: 40.0,
+                        color: Color::rgb(0.8, 0.8, 0.8),
+                    },
+                },
+                ..Default::default()
+            });
+        });
+}
